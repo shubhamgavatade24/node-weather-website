bc2b61b67a3a6a4fbfe1725278c7bed82ec31c27
redwinner24 <redwinner24@gmail.com>
2020-11-09 14:18:48 +0530

    Added high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,9 +10,9 @@ const forecast = (latitude , longitude ,callback) => {
         else if(body.error){
             callback('Unable to find location')
         }
-        else {
+        else {            
             const curr = body.currently
-            callback(undefined,body.daily.data[0].summary +' It is currently ' + curr.temperature + ' degrees out. There is a ' + curr.precipProbability + '% chance of rain')    
+            callback(undefined,body.daily.data[0].summary +' It is currently ' + curr.temperature + ' degrees out. The high today is '+ body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow +'. There is a ' + curr.precipProbability + '% chance of rain')    
         }
     })
 }
